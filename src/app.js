ac329efae8924b3ad2b0488fc14e23a409a50a84
Michał Gacek <gacek.michael@outlook.com>
2023-02-15 19:01:47 +0100

    feat: add flowbite components (appbar, weatherform)

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+const successGeolocation = (position) => {
+  const { latitude, longitude } = position.coords;
+
+  console.log(latitude, longitude);
+};
+
+const errorGeolocation = () => {
+  console.log("Geolocation API is not available.");
+};
+
+navigator.geolocation.getCurrentPosition(successGeolocation, errorGeolocation);
